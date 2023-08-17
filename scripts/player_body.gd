3c9539092a2768627268e00e3ebf76f905377c68
Jacob Torrång <jacob@juni.co>
2023-08-17 21:59:31 +0200

    Make jump height variable

--- scripts/player_body.gd
+++ scripts/player_body.gd
@@ -1,7 +1,11 @@
 extends CharacterBody2D
 
+var is_jumping = false
+
 const SPEED = 160
-const JUMP_VELOCITY = 320
+const JUMP_STOP_SPEED = -60
+const JUMP_VELOCITY = -320
+const MAX_FALL_SPEED = 180
 
 signal ate_chocolate(c: String)
 
@@ -20,13 +24,19 @@ func _physics_process(delta):
 	apply_inputs()
 	move_and_slide()
 
-
 func apply_inputs():
 	if Input.is_action_just_pressed("restart_level"):
 		get_tree().reload_current_scene()
 
+	if is_on_floor():
+		is_jumping = false
+	
 	if is_on_floor() and Input.is_action_just_pressed("jump"):
-		velocity.y = -JUMP_VELOCITY
+		velocity.y = JUMP_VELOCITY
+		is_jumping = true
+		
+	if is_jumping and Input.is_action_just_released("jump"):
+		velocity.y = max(velocity.y, JUMP_STOP_SPEED) 
 
 	var direction = Input.get_axis("move_left", "move_right")
 	velocity.x = direction * SPEED
@@ -36,7 +46,7 @@ func apply_inputs():
 
 func apply_gravity(delta: float):
 	if not is_on_floor():
-		velocity.y += gravity * delta
+		velocity.y = min(velocity.y + gravity * delta, MAX_FALL_SPEED)
 	else:
 		velocity.y = 0
 
