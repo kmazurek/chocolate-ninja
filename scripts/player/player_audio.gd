93029ae671a27bd0429a0e4c9ead5a797f55611f
Kuba Mazurek <zakaprov@gmail.com>
2023-08-18 00:44:24 +0200

    Connect munch signal through code

--- scripts/player/player_audio.gd
+++ scripts/player/player_audio.gd
@@ -2,6 +2,10 @@ extends AudioStreamPlayer
 
 @onready var chomp_stream = load("res://assets/audio/sfx/ninja/ninja-chomp.tres")
 
+func _ready():
+	var player = get_parent()
+	player.connect("ate_chocolate", _on_player_ate_chocolate)
+
 func _on_player_ate_chocolate(c: String):
 	stream = chomp_stream
 	play()
