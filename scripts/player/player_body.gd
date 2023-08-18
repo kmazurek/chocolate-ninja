4ef84ad431ab18db27bc0e151151d56e83211f82
Jacob Torrång <jacob@juni.co>
2023-08-18 19:29:31 +0200

    Before I go

--- scripts/player/player_body.gd
+++ scripts/player/player_body.gd
@@ -1,14 +1,17 @@
 extends CharacterBody2D
 
 var is_jumping = false
+var is_falling_through = false
 
 const SPEED = 130
 const JUMP_STOP_SPEED = -60
 const JUMP_VELOCITY = -260
 const MAX_FALL_SPEED = 180
+const JUMP_DOWN_VELOCITY = -80
 
 
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
+@onready var tile_map: TileMap = get_parent().get_parent()
 
 func _physics_process(delta):
 	apply_gravity(delta)
@@ -20,16 +23,30 @@ func apply_inputs():
 	if Input.is_action_just_pressed("restart_level"):
 		get_tree().reload_current_scene()
 
+	var is_fallthrough = not get_collision_layer_value(3)
+
 	if is_on_floor():
 		is_jumping = false
-	
-	if is_on_floor() and Input.is_action_just_pressed("jump"):
+		set_collision_layer_value(3, true)
+		
+	if is_on_floor() and Input.is_action_pressed("move_down") and Input.is_action_just_pressed("jump"):
+		velocity.y = JUMP_DOWN_VELOCITY
+		is_jumping = true
+		set_collision_layer_value(3, false)
+		set_collision_mask_value(3, false)
+	elif is_on_floor() and Input.is_action_just_pressed("jump"):
 		velocity.y = JUMP_VELOCITY
 		is_jumping = true
 		
-	if is_on_floor() and Input.is_action_just_pressed("move_down"):
-		velocity.y = -JUMP_VELOCITY
-		is_jumping = true
+	if is_on_fallthrough() and is_fallthrough:
+		is_falling_through = true
+	elif not is_on_fallthrough() and is_falling_through:
+		set_collision_layer_value(3, true)
+		is_falling_through = false
+		
+		
+		
+
 		
 	if is_jumping and Input.is_action_just_released("jump"):
 		velocity.y = max(velocity.y, JUMP_STOP_SPEED) 
@@ -40,6 +57,21 @@ func apply_inputs():
 	if direction != 0:
 		$Animations/AnimatedSpriteLight.flip_h = direction != 1
 		$Animations/AnimatedSpriteDark.flip_h = direction != 1
+
+func is_on_fallthrough():
+	var coordinates = tile_map.local_to_map(global_position)
+	
+	var data = tile_map.get_cell_tile_data(0, coordinates)
+	if data == null: return false
+	
+	var custom_data = data.get_custom_data_by_layer_id(0)
+	return custom_data == "fallthrough"
+		
+	
+	
+func set_fallthrough(b: bool): 
+	set_collision_layer_value(3, b)
+	set_collision_mask_value(3, b)
 
 func apply_gravity(delta: float):
 	if not is_on_floor():
