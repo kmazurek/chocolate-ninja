cb8e18e974b1e459d770e3935db91235dd903ff1
Kuba Mazurek <zakaprov@gmail.com>
2023-08-19 19:59:28 +0200

    Add busted SFX

--- scripts/player/player_audio.gd
+++ scripts/player/player_audio.gd
@@ -1,11 +1,17 @@
 extends AudioStreamPlayer
 
 @onready var chomp_stream = load("res://assets/audio/sfx/ninja/ninja-chomp.tres")
+@onready var busted_stream = load("res://assets/audio/sfx/ninja/ninja-busted.tres")
 
 func _ready():
 	var player = get_parent()
 	player.connect("ate_chocolate", _on_player_ate_chocolate)
+	player.connect("got_seen", _on_player_got_seen)
 
 func _on_player_ate_chocolate(c: String):
 	stream = chomp_stream
+	play()
+
+func _on_player_got_seen(_position):
+	stream = busted_stream
 	play()
